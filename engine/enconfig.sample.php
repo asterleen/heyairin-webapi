6be2720b26addc4d9ee289fe85c6435178d8cf3d
Akito Iwakura <aki@asterleen.com>
2018-09-24 15:44:21 +0300

    Actualize the enconfig sample file

--- engine/enconfig.sample.php
+++ engine/enconfig.sample.php
@@ -15,6 +15,8 @@
 
 // General
 define ('BASE_DOMAIN', 'example.com');
+define ('LOCATION_PREFIX', 'heyairin');
+define ('FRONTEND_ADDRESS', 'https://lab.nyan.pw/heyairin/webapp/#auth:%s');
 
 // Database
 define ('DB_HOST', 'localhost');
@@ -25,18 +27,16 @@ define ('DB_PASSWORD', 'hackme');
 // VK Auth
 define ('VK_CLIENT_ID', 31337);
 define ('VK_CLIENT_SECRET', '_top_secret_');
-define ('VK_REDIRECT_URI', 'https://'.BASE_DOMAIN.'/api/auth/vk');
+define ('VK_REDIRECT_URI', 'https://'.BASE_DOMAIN.'/'.LOCATION_PREFIX.'/auth/vk');
 define ('VK_API_VERSION', '5.73');
 
 // Facebook Auth
 define ('FB_CLIENT_ID', 31337);
 define ('FB_CLIENT_SECRET', '_top_secret_');
-define ('FB_REDIRECT_URI', 'https://'.BASE_DOMAIN.'/api/auth/fb');
+define ('FB_REDIRECT_URI', 'https://'.BASE_DOMAIN.'/'.LOCATION_PREFIX.'/auth/fb');
 
 
 // System settings
 define ('DEFAULT_AMOUNT', 50);
 define ('MAX_MESSAGE_AMOUNT', 1024);
 define ('TEMP_CODE_SALT', 'change_this_to_pure_randomness');
-define ('CURRENT_TRACK_FILE', '');
-define ('CURRENT_TAG_FILE', '');
